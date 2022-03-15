cf9324bfc1f922b3d826f83d7b31db8cd0150dd3
Robin Sommer <robin@corelight.com>
2022-03-15 08:58:14 +0100

    Fix potential deadlock in unit test.
    Our query fixture could deadlock if we it didn't get the expected
    results. Removing the condition variable, which is no longer needed
    with the reworked threading structure.

--- src/util/testing.h
+++ src/util/testing.h
@@ -54,21 +54,16 @@ public:
      * @returns a valid query result (any prior errors are caught and lead to abortion)
      */
     query::Result query(std::string stmt) {
-        ConditionVariable cv;
         std::optional<query::Result> result;
         Query q = {.sql_stmt = std::move(stmt),
                    .subscription = {},
                    .cookie = "",
-                   .callback_result = [&](query::ID id, query::Result result_) {
-                       result = std::move(result_);
-                       cv.notify();
-                   }};
+                   .callback_result = [&](query::ID id, query::Result result_) { result = std::move(result_); }};
 
         auto rc = db.query(q);
         REQUIRE_MESSAGE(rc, rc.error());
 
         scheduler.advance(scheduler.currentTime() + 1s); // this will execute the query
-        cv.wait();                                       // probably unnecessary to deploy a CV here, but safer
 
         REQUIRE(result);
         return std::move(*result);
