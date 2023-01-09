caff44377ef6a240b59099b8ead1dc1d346ae056
Robin Sommer <robin@corelight.com>
2023-01-09 16:52:16 +0100

    Add BPF-based `sockets_events` table on Linux.
    This is still experimental and may require further tuning to improve
    the data being returned.

--- src/tables/sockets/sockets.h
+++ src/tables/sockets/sockets.h
@@ -33,4 +33,34 @@ public:
         };
     }
 };
+
+class SocketsEventsCommon : public EventTable {
+public:
+    Schema schema() const override {
+        return {
+            // clang-format off
+            .name = "sockets_events",
+            .summary = "open network sockets",
+            .description = R"(
+                The table reports IP sockets opening and closing on the endpoint.
+                )",
+            .platforms = { Platform::Linux },
+            .columns = {
+                {.name = "time", .type = value::Type::Time, .summary = "timestamp"},
+                {.name = "pid", .type = value::Type::Count, .summary = "ID of process holding socket"},
+                {.name = "process", .type = value::Type::Text, .summary = "name of process holding socket"},
+                {.name = "uid", .type = value::Type::Count, .summary = "user ID of process"},
+                {.name = "gid", .type = value::Type::Count, .summary = "group ID of process"},
+                {.name = "family", .type = value::Type::Text, .summary = "`IPv4` or `IPv6`"},
+                {.name = "protocol", .type = value::Type::Count, .summary = "transport protocol"},
+                {.name = "local_addr", .type = value::Type::Address, .summary = "local IP address"},
+                {.name = "local_port", .type = value::Type::Count, .summary = "local port number"},
+                {.name = "remote_addr", .type = value::Type::Address, .summary = "remote IP address"},
+                {.name = "remote_port", .type = value::Type::Count, .summary = "remote port number"},
+                {.name = "state", .type = value::Type::Text, .summary = "state of socket"},
+        }
+            // clang-format on
+        };
+    }
+};
 } // namespace zeek::agent::table
--- src/tables/sockets/sockets.linux.cc
+++ src/tables/sockets/sockets.linux.cc
@@ -1,4 +1,3 @@
-// Copyright (c) 2021 by the Zeek Project. See LICENSE for details.
 
 #include "sockets.h"
 
@@ -6,10 +5,17 @@
 #include "core/database.h"
 #include "core/logger.h"
 #include "core/table.h"
+#include "sockets.linux.event.h"
 #include "util/fmt.h"
 #include "util/helpers.h"
 
+// clang-format off
+#include "platform/linux/bpf.h"
+#include "autogen/bpf/sockets.skel.h"
+// clang-format on
+
 #include <arpa/inet.h>
+#include <linux/bpf.h>
 #include <netinet/in.h>
 #include <pfs/procfs.hpp>
 
@@ -112,4 +118,118 @@ std::vector<std::vector<Value>> SocketsLinux::snapshot(const std::vector<table::
 
     return rows;
 }
+
+class SocketsEventsLinux : public SocketsEventsCommon {
+public:
+    bool init() override;
+    void activate() override;
+    void deactivate() override;
+};
+
+namespace {
+database::RegisterTable<SocketsEventsLinux> _2;
+}
+
+template<typename T, typename S>
+Value to_val(const S& i) {
+    return i ? Value(static_cast<T>(i)) : Value();
+}
+
+static int handle_event(void* ctx, void* data, size_t data_sz) {
+    static auto addr_to_string = [](const void* addr, uint64_t family) -> Value {
+        switch ( family ) {
+            case AF_INET:
+                if ( memcmp(addr, "\x00\x00\x00\x00", 4) == 0 )
+                    return {};
+                break;
+
+            case AF_INET6:
+                if ( memcmp(addr, "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00",
+                            16) == 0 )
+                    return {};
+
+                break;
+
+            default: break;
+        }
+
+        char buffer[INET6_ADDRSTRLEN];
+        return inet_ntop(static_cast<int>(family), addr, buffer, sizeof(buffer));
+    };
+
+    auto table = reinterpret_cast<SocketsEventsLinux*>(ctx);
+    auto ev = reinterpret_cast<const bpfSocketEvent*>(data);
+
+    auto pid = to_val<int64_t>(ev->process.pid);
+    auto uid = to_val<int64_t>(ev->process.uid);
+    auto gid = to_val<int64_t>(ev->process.gid);
+    auto process = Value(ev->process.name);
+
+    Value family;
+    switch ( ev->family ) {
+        case AF_INET: family = "IPv4"; break;
+        case AF_INET6: family = "IPv6"; break;
+        default: family = frmt("family-{}", ev->family);
+    }
+
+    auto protocol = to_val<int64_t>(ev->protocol);
+    auto local_addr = addr_to_string(&ev->local_addr, ev->family);
+    auto local_port = to_val<int64_t>(ev->local_port);
+    auto remote_addr = addr_to_string(&ev->remote_addr, ev->family);
+    auto remote_port = to_val<int64_t>(ev->remote_port);
+
+    Value state;
+    switch ( ev->state ) {
+        case BPF_SOCKET_STATE_CLOSED: state = "closed"; break;
+        case BPF_SOCKET_STATE_ESTABLISHED: state = "established"; break;
+        case BPF_SOCKET_STATE_EXPIRED: state = "expired"; break;
+        case BPF_SOCKET_STATE_FAILED: state = "failed"; break;
+        case BPF_SOCKET_STATE_LISTEN: state = "listen"; break;
+        case BPF_SOCKET_STATE_UNKNOWN: break; // leave unset
+    }
+
+    table->newEvent({table->systemTime(), pid, process, uid, gid, family, protocol, local_addr, local_port, remote_addr,
+                     remote_port, state});
+
+    return 1;
+}
+
+bool SocketsEventsLinux::init() {
+    auto bpf = platform::linux::bpf();
+    if ( ! bpf->isAvailable() )
+        return false;
+
+    auto skel = platform::linux::BPF::Skeleton{.name = "Sockets",
+                                               .open = reinterpret_cast<void*>(sockets__open),
+                                               .load = reinterpret_cast<void*>(sockets__load),
+                                               .attach = reinterpret_cast<void*>(sockets__attach),
+                                               .detach = reinterpret_cast<void*>(sockets__detach),
+                                               .destroy = reinterpret_cast<void*>(sockets__destroy),
+                                               .event_callback = handle_event,
+                                               .event_context = this};
+
+    auto our_bpf = bpf->load<sockets>(std::move(skel));
+    if ( ! our_bpf ) {
+        logger()->warn(frmt("could not load BPF program: {}", our_bpf.error()));
+        return false;
+    }
+
+    if ( auto rc = bpf->init("Sockets", (*our_bpf)->maps.ring_buffer); ! rc ) {
+        logger()->warn(frmt("could not initialize BPF program: {}", our_bpf.error()));
+        return false;
+    }
+
+    return true;
+}
+
+void SocketsEventsLinux::activate() {
+    if ( auto rc = platform::linux::bpf()->attach("Sockets"); ! rc )
+        logger()->error(frmt("could not attach BPF program: {}", rc.error()));
+}
+
+void SocketsEventsLinux::deactivate() {
+    if ( auto rc = platform::linux::bpf()->detach("Sockets"); ! rc )
+        logger()->error(frmt("could not detach BPF program: {}", rc.error()));
+}
+
 } // namespace zeek::agent::table
